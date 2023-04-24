23fc6686661d426aa201aa115787ee0e98bd5223
szintia <kasziba.cintia@gmail.com>
2023-04-24 12:49:59 +0200

    added React app created based on freecodecamp training

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Main() {
+    return (
+       <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Topic 1</li>
+                <li>Topic 2</li>
+                <li>Topic 3</li>
+                <li>Topic 4</li>
+            </ul>
+       </main>
+    )
+}
+
+
