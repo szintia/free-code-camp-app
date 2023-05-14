4fc9fa32329fc8e8316d8c15e5160ddbaa19e19a
szintia <kasziba.cintia@gmail.com>
2023-05-14 13:12:47 +0200

    added shareable style objects to airbnb navbar

--- src/components/content/AirBnbNavbar.js
+++ src/components/content/AirBnbNavbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AirBnbLogo from "../../images/airbnb-logo.png";
+import {airBnbNavbarStyles} from "../styles/airBnbNavbarStyles";
+
 
 function AirBnbNavbar() {
     return (
-        <nav>
-            <img src={AirBnbLogo} />
+        <nav style={airBnbNavbarStyles.Navbar}>
+            <img src={AirBnbLogo} alt="" style={airBnbNavbarStyles.Logo}/>
         </nav>
     )
 }
---
+++ src/components/styles/airBnbNavbarStyles.js
@@ -0,0 +1,14 @@
+
+const Navbar = {
+    backgroundColor : "white"
+};
+
+const Logo = {
+    display : "flex",
+    height: "70px"
+};
+
+export const airBnbNavbarStyles = {
+    Navbar : Navbar,
+    Logo : Logo
+}
